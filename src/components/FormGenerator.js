060576f02f1258bb78971c16ef1880dc9e0fa69c
Dinakaran S <dinakaran@nobel-systems.com>
2021-06-01 19:27:08 +0530

    onChange function

--- src/components/FormGenerator.js
+++ src/components/FormGenerator.js
@@ -24,15 +24,23 @@ const handleSubmit = (callback, data) => {
 /** FormGenerator */
 export const FormGenerator = (props) => {
   try {
-    const { data = [], patch = {}, guid, formRef, onSubmit } = props;
+    const { data = [], patch = {}, guid, formRef, onSubmit, onChange } = props;
     const config = LIBMap.MUI;
     const dataObj = JSON.parse(JSON.stringify(data));
     const layout = generateLayout(
       updatePatchData(JSON.parse(JSON.stringify(dataObj)), patch, guid)
     );
-    const onChange = ({ id, value }) => {
-      if (isEmpty(response[guid])) response[guid] = patch;
-      response[guid][id] = value;
+    const onUpdate = ({ id, value }) => {
+      try {
+        if (isEmpty(response[guid])) response[guid] = patch;
+        response[guid][id] = value;
+        if (typeof onChange === 'function') {
+          onChange({ id, value });
+        }
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+      }
     };
 
     return (
@@ -67,7 +75,7 @@ export const FormGenerator = (props) => {
                       control={field}
                       attributes={cProps}
                       rules={rules}
-                      onChange={onChange}
+                      onChange={onUpdate}
                     />
                   </Grid>
                 );
@@ -87,11 +95,13 @@ export const FormGenerator = (props) => {
               className={`${className} ${visible === false ? 'hidden' : 'show'}`}
             >
               <DynamicComponent
+                key={generateKey('dynamic-comp', index)}
                 map={configObj.map}
                 option={options.type || ''}
                 control={field}
                 attributes={cProps}
                 rules={rules}
+                onChange={onUpdate}
               />
             </div>
           );
@@ -122,6 +132,12 @@ FormGenerator.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   /** Json data to assign value */
   patch: PropTypes.objectOf(PropTypes.object),
+  /** Component Ref */
+  formRef: PropTypes.objectOf(PropTypes.object),
+  /** Component Submit Function */
+  onSubmit: PropTypes.func,
+  /** Component On Change Function */
+  onChange: PropTypes.func,
 };
 
 FormGenerator.defaultProps = {
