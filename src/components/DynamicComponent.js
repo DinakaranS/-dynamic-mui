f24ff90040f640f576198169a5022af84b7a948d
dinakaran <dinakaran@nobel-systems.com>
2024-03-13 18:16:45 +0530

    crashing bug fix

--- src/components/DynamicComponent.js
+++ src/components/DynamicComponent.js
@@ -4,6 +4,7 @@ import * as Controls from './controls/index';
 
 export default function DynamicComponent(props) {
   const { map } = props;
+  if (!map) return <div />;
   const CustomComponent = Controls.default[map];
   return <CustomComponent {...props} />;
 }
