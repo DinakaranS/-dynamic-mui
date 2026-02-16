f1f0526d9d69bbb9b85f0d7d55fbc1cd958f1772
dinakaran <dinakaran@nobel-systems.com>
2026-02-16 08:16:15 +0530

    Upgraded dep

--- src/components/controls/Select/select.js
+++ src/components/controls/Select/select.js
@@ -11,16 +11,38 @@ import { checkboxSX, getInputProps } from '../../../util/helper';
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
-const getValue = (options = [], defaultValue = '', isMultiple = false) => {
-  if (isMultiple) {
-    let separator = ',';
-    if (defaultValue?.includes(',')) separator = ',';
-    else if (defaultValue?.includes(';')) separator = ';';
-    const dValueSet = new Set(defaultValue?.split(separator));
-    return options?.filter(({ value }) => dValueSet?.has(value));
-  }
+const getValue = (options = [], defaultValue = '', isMultiple = false, separator = ',') => {
+  try {
+    const safeOptions = Array.isArray(options) ? options : [];
 
-  return options.find(({ value }) => value === defaultValue);
+    if (isMultiple) {
+      let values = [];
+
+      if (Array.isArray(defaultValue)) {
+        values = defaultValue;
+      } else if (typeof defaultValue === 'string' && defaultValue.trim() !== '') {
+        let tempSeparator = separator;
+        if (defaultValue.includes(',')) tempSeparator = ',';
+        else if (defaultValue.includes(';')) tempSeparator = ';';
+
+        values = defaultValue.split(tempSeparator).map((v) => v.trim());
+      }
+
+      const dValueSet = new Set(values);
+      return safeOptions.filter(({ value }) => dValueSet.has(value));
+    }
+
+    if (Array.isArray(defaultValue)) {
+      const first = defaultValue[0];
+      return safeOptions.find(({ value }) => value === first);
+    }
+
+    return safeOptions.find(({ value }) => value === defaultValue);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('getValue error:', err);
+    return isMultiple ? [] : null;
+  }
 };
 
 export default function Select({ attributes, onChange }) {
@@ -32,7 +54,8 @@ export default function Select({ attributes, onChange }) {
     InputProps = {},
   } = attributes;
   const [value, setValue] = React.useState(
-    attributes?.value && getValue(options, attributes?.value, MuiAttributes.multiple),
+    attributes?.value &&
+      getValue(options, attributes?.value, MuiAttributes.multiple, attributes?.separator),
   );
   const getMuiAttributes = () => {
     if (MuiAttributes.multiple) {
@@ -54,13 +77,41 @@ export default function Select({ attributes, onChange }) {
 
   const extractValue = (option) => option?.value || option?.title || option?.label;
 
-  const onChangeEvent = useCallback((event, newValue) => {
-    setValue(newValue);
-    if (newValue) {
-      const data = MuiAttributes.multiple ? newValue.map(extractValue) : extractValue(newValue);
-      onChange({ id, value: MuiAttributes.multiple ? data.toString() : data, option: newValue });
-    } else onChange({ id, value: '', option: newValue });
-  }, []);
+  const onChangeEvent = useCallback(
+    (event, newValue) => {
+      setValue(newValue);
+
+      // Use attributes?.separator, default to ';'
+      const separator = attributes?.separator ?? ';';
+
+      if (newValue) {
+        const data = MuiAttributes.multiple ? newValue.map(extractValue) : extractValue(newValue);
+
+        const customValue = MuiAttributes.multiple
+          ? data.join(separator) // <-- use custom separator here
+          : data;
+
+        onChange({
+          id,
+          value: customValue,
+          option: newValue,
+        });
+      } else {
+        onChange({
+          id,
+          value: '',
+          option: newValue,
+        });
+      }
+    },
+    [
+      id,
+      onChange,
+      attributes?.separator, // so changes to separator update the callback
+      MuiAttributes.multiple,
+      extractValue,
+    ],
+  );
 
   return (
     <Autocomplete
